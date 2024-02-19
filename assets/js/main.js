c013ac1aff5f69909ee4fc120399ec6e93ad6807
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-19 15:58:45 +0100

    Add constant from users with prompt

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Get variables from user with prompt
+
+const total_km = prompt("Inserisci il numero totale di chilometri da percorrere (km)")
+const user_age = prompt( "Inserisci la tua età")
+
+console.log(total_km);
+console.log(user_age);
+
+
+
