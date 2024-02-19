e5a64ece0321b7c504cfed7b31a7196420a345a8
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-19 16:00:00 +0100

    Add variabel for ticket amount

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,12 @@ const user_age = prompt( "Inserisci la tua età")
 console.log(total_km);
 console.log(user_age);
 
+// Calculate the cost of tickets based on the amount of kilometres
+
+const cost_per_km = 0.21;
+let ticket_Amount = Math.floor(total_km * cost_per_km);
+
+console.log(ticket_Amount);
+
 
 
