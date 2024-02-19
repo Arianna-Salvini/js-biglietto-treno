c9114704d30e8d0d39a0049f55132f3f47cd7297
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-19 16:34:59 +0100

    Add variables for discount to view the operation and use a variable instead of use only its result in variables for  tickets discounted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Get variables from user with prompt
 
-const total_km = prompt("Inserisci il numero totale di chilometri da percorrere (km)")
-const user_age = prompt( "Inserisci la tua età")
+const total_km = prompt("Inserisci il numero totale di chilometri da percorrere (km)");
+const user_age = prompt( "Inserisci la tua età");
 
 console.log(total_km);
 console.log(user_age);
@@ -9,14 +9,17 @@ console.log(user_age);
 // Calculate the cost of tickets based on the amount of kilometres
 
 const cost_per_km = 0.21;
-let ticket_amount =(total_km * cost_per_km).toFixed(2);
+const ticket_amount =(total_km * cost_per_km).toFixed(2);
 
 console.log(ticket_amount);
 
 // Discount (amount/100 * discount = amount discounted)
 
-let amount_under18 =(ticket_amount * 0.8).toFixed(2);
-let amount_over65 = (ticket_amount * 0.6).toFixed(2);
+let under18_discount = 80 / 100;
+let over65_discount = 60 / 100;
+
+const amount_under18 =(ticket_amount * under18_discount).toFixed(2);
+const amount_over65 = (ticket_amount * over65_discount).toFixed(2);
 
 console.log(amount_under18);
 console.log(amount_over65);
